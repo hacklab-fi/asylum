c05e89f9496955bd40aadf2465efed860d6c616b
Eero af heurlin <eero.afheurlin@iki.fi>
2015-11-28 01:37:19 +0200

    initial models

--- project/members/admin.py
+++ project/members/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from reversion.admin import VersionAdmin
+from .models import MemberType, Member, MembershipApplication
 
-# Register your models here.
+
+class MemberTypeAdmin(VersionAdmin):
+    pass
+
+
+class MemberAdmin(VersionAdmin):
+    pass
+
+
+class MembershipApplicationAdmin(VersionAdmin):
+    pass
+
+
+admin.site.register(MemberType, MemberTypeAdmin)
+admin.site.register(Member, MemberAdmin)
+admin.site.register(MembershipApplication, MembershipApplicationAdmin)
