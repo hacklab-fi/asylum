fd4747d19cd1932270e8f8369c1ef0a211af8ba2
Eero af heurlin <eero.afheurlin@iki.fi>
2015-11-28 18:12:48 +0200

    Require applicant to accept rules, fixes #29
    Also added a new config key for the rules URL that can be simply set via .env

--- project/members/views.py
+++ project/members/views.py
@@ -3,19 +3,13 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
 from .models import MembershipApplication
+from .forms import ApplicationForm
 
 
 class ApplyView(generic.CreateView):
     model = MembershipApplication
     template_name = "members/application_form.html"
-    fields = [
-        'fname',
-        'lname',
-        'city',
-        'email',
-        'phone',
-        'nick',
-    ]
+    form_class = ApplicationForm
 
     def get_success_url(self):
         return reverse('members-application_received')
