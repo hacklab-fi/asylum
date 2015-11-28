dd8ab9419ab292a29161212145bed9a622c25fe9
Eero af heurlin <eero.afheurlin@iki.fi>
2015-11-29 01:17:11 +0200

    Simple CSV members importer, fixes #50
    And a management command to pass a file to it.
    CSVFile first row headers must match Member model
    properties to be imported

---
+++ project/members/importer.py
@@ -0,0 +1,47 @@
+import csv
+from .models import Member
+import logging
+logger = logging.getLogger('members.importer')
+
+
+class importer(object):
+
+    def __init__(self, fp):
+        self.reader = csv.reader(fp, delimiter=';', quotechar='"')
+        headers = next(self.reader)
+        self.header_map = {}
+        for x in range(len(headers)):
+            prop = headers[x]
+            m = Member()
+            if not hasattr(m, prop):
+                msg = "%s not matched to member fields" % prop
+                logger.warning(msg)
+                print(msg)
+                continue
+            self.header_map[prop] = x
+
+    def import_members(self):
+        for row in self.reader:
+            # Incomplete row
+            if len(row) < max(self.header_map.values()):
+                continue
+            m = Member()
+            for prop in self.header_map:
+                v = row[self.header_map[prop]]
+                if prop == 'member_id':
+                    if v:
+                        m.member_id = int(v)
+                elif prop == 'anonymized_id':
+                    if v:
+                        m.anonymized_id = v
+                else:
+                    setattr(m, prop, v)
+            try:
+                m.save()
+                msg = "Member %s (#%d) added" % (m, m.pk)
+                logger.info(msg)
+                print(msg)
+            except Exception as err:
+                msg = "Got error %s when adding member" % err
+                logger.error(msg)
+                print(msg)
