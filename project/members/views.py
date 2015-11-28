5c2fb6b79b80d5f9d8474e82ad0e65af2ba5c8d0
Eero af heurlin <eero.afheurlin@iki.fi>
2015-11-28 17:43:28 +0200

    redirect to application form from members home

--- project/members/views.py
+++ project/members/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
-
 from .models import MembershipApplication
+
 
 class ApplyView(generic.CreateView):
     model = MembershipApplication
@@ -20,6 +20,12 @@ class ApplyView(generic.CreateView):
     def get_success_url(self):
         return reverse('members-application_received')
 
+
 class ApplicationReceivedView(generic.TemplateView):
     template_name = "members/application_received.html"
+
+
+class HomeView(generic.base.RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('members-apply')
 
