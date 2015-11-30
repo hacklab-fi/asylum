fccbf560d8085a522c1a61033c959714541a56b7
Otso Jousimaa <otso@ojousima.net>
2015-12-01 01:36:19 +0200

    Enable locale middleware

--- project/config/settings/common.py
+++ project/config/settings/common.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE_CLASSES = (
     # Make sure djangosecure.middleware.SecurityMiddleware is listed first
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
