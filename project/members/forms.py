873662de5131aa97fc1f572435b7d1914acb166c
Eero af heurlin <eero.afheurlin@iki.fi>
2015-12-01 23:00:43 +0200

    Fix the localization for the rules checkbox label

--- project/members/forms.py
+++ project/members/forms.py
@@ -1,10 +1,17 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext
+from django.utils.functional import lazy, allow_lazy
 from .models import MembershipApplication
 from django.conf import settings
 
+def rules_accepted_proxy(msg):
+    return msg % settings.APPLICATION_RULES_URL
+
+rules_accepted_proxy = allow_lazy(rules_accepted_proxy, str)
+
 class ApplicationForm(forms.ModelForm):
-    rules_accepted = forms.BooleanField(required=True, label=_("I have read and accept <a href='%s' target='_blank'>the rules</a>") % settings.APPLICATION_RULES_URL)
+    rules_accepted = forms.BooleanField(required=True, label=rules_accepted_proxy(_("I have read and accept <a href=\"%s\" target=\"_blank\">the rules</a>")) )
 
     class Meta:
         model = MembershipApplication
