882ce4cc9584644681bb51b7034063117a52ae80
Eero af heurlin <eero.afheurlin@iki.fi>
2015-12-01 20:46:02 +0200

    read .env on manage.py too

--- project/manage.py
+++ project/manage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import environ
 
 if __name__ == "__main__":
+    if os.path.isfile('.env'):
+        environ.Env.read_env('.env')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
     from django.core.management import execute_from_command_line
