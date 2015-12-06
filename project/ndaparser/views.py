da317d0d0336a9ae1ac290bf11873390c4bcbe88
Eero af Heurlin <rambo@iki.fi>
2015-12-06 22:29:19 +0200

    Parse the uploaded transactions file
    TODO: pass the parsed data on to something that
    can make creditor.Transaction objects out of them

--- project/ndaparser/views.py
+++ project/ndaparser/views.py
@@ -1,6 +1,10 @@
+import tempfile
+import os
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import FormView
 from .forms import UploadForm
+from .ndaparser import parseLine
+
 
 class NordeaUploadView(FormView):
     form_class = UploadForm
@@ -13,7 +17,26 @@ class NordeaUploadView(FormView):
 
     def form_valid(self, form):
         context = self.get_context_data()
-        print("File received, FILES=%s" % repr(self.request.FILES))
-        # TODO parse and handle, update context accordingly
+        # We have to write out the file in binary mode before we properly parse it by lines
+        tmp = tempfile.NamedTemporaryFile(prefix="ndaupload", delete=False)
+        with tmp as dest:
+            for chunk in self.request.FILES['ndafile'].chunks():
+                dest.write(chunk)
+
+        transactions = []
+        with open(tmp.name) as f:
+            for line in f:
+                transaction = parseLine(line)
+                if transaction is not None:
+                    # TODO: Check if there is transaction mapper defined, if so call it
+                    transactions.append(transaction)
+                else:
+                    # Raise error ? AFAIK there should be no unparseable lines
+                    pass
+
+        # Done with the temp file, get rid of it
+        os.unlink(tmp.name)
+
         context['title'] = _("Transactions uploaded")
+        context['transactions'] = transactions
         return self.render_to_response(context)
