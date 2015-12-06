34bafa90e7eaa1e56a2a29421d97751ba91368a9
Eero af Heurlin <rambo@iki.fi>
2015-12-06 21:52:09 +0200

    fix context handling, do not try to show form if it is not defined

--- project/ndaparser/views.py
+++ project/ndaparser/views.py
@@ -1,3 +1,4 @@
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import FormView
 from .forms import UploadForm
 
@@ -5,14 +6,14 @@ class NordeaUploadView(FormView):
     form_class = UploadForm
     template_name = "ndaparser/admin/upload.html"
 
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context.update(self.kwargs['context'])
         return context
 
     def form_valid(self, form):
-        context = self.get_context_data(form)
-        print("File received")
+        context = self.get_context_data()
+        print("File received, FILES=%s" % repr(self.request.FILES))
         # TODO parse and handle, update context accordingly
         context['title'] = _("Transactions uploaded")
         return self.render_to_response(context)
