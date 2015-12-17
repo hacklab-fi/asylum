1daeebd7b0b8b5c08559610bac377dc3bb171b9e
Eero af Heurlin <rambo@iki.fi>
2015-12-17 22:53:48 +0200

    Make sure the generated email is valid
    Also add a note.

--- project/members/management/commands/generate_members.py
+++ project/members/management/commands/generate_members.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from django.core.management.base import BaseCommand, CommandError
-from members.importer import importer
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 import factory.django, factory.fuzzy
+from members.models import generate_unique_memberid
 
 import os, codecs
 
@@ -16,19 +18,27 @@ firstnames = [
     codecs.open(os.path.join(RANDOM, 'females'), 'r' ,'utf-8').readlines()
 ]
 
+
+def generate_email(memberlike):
+    try:
+        addr = '%s.%s@hacklab.hax' % (
+            memberlike.fname.encode('ascii','ignore').decode('utf-8').lower(),
+            memberlike.lname.encode('ascii','ignore').decode('utf-8').lower()
+        )
+        validate_email(addr)
+        return addr
+    except ValidationError as e:
+        return 'member_%d@hacklab.hax' % generate_unique_memberid()
+
+
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'members.Member'  # Equivalent to ``model = myapp.models.User``
         django_get_or_create = ('fname', 'lname', 'email', 'city')
     fname = factory.fuzzy.FuzzyChoice(firstnames)
     lname = factory.fuzzy.FuzzyChoice(lastnames)
-    email = factory.LazyAttribute(
-        lambda o: '%s.%s@hacklab.hax' % (
-            o.fname.encode('ascii','ignore').decode('utf-8').lower(),
-            o.lname.encode('ascii','ignore').decode('utf-8').lower()
-        )
-    )
-    city = 'Helsinki'
+    email = factory.LazyAttribute(generate_email)
+    city = 'Helsinki' # TODO: Randomize city ?
 
 class Command(BaseCommand):
     help = 'generate randomised members'
