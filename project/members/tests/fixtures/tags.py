b88073110c40e65fe369afc0f346f2428459039c
Eero af Heurlin <rambo@iki.fi>
2015-12-22 19:38:08 +0200

    Add fixtures for MemberTypes and MembershipApplicationTags
    Refs #116

---
+++ project/members/tests/fixtures/tags.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import factory.django, factory.fuzzy
+
+class MembershipApplicationTagFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'members.MembershipApplicationTag'
+        django_get_or_create = ('label',)
+
+
+def generate_standard_set():
+    for x in ('Welcome email sent', 'Special tag 1', 'Special tag 2'):
+        MembershipApplicationTagFactory(label=x)
