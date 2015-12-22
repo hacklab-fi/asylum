cf5dda5aa93587ec3e09d97f87bf81f020d4c49a
Eero af Heurlin <rambo@iki.fi>
2015-12-22 21:24:42 +0200

    make the automatic times editable so our fixtures have more variance

---
+++ project/members/migrations/0004_auto_20151229_2055.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('members', '0003_auto_20151206_1908'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='member',
+            options={'verbose_name': 'Member', 'verbose_name_plural': 'Members'},
+        ),
+        migrations.AlterModelOptions(
+            name='membernote',
+            options={'verbose_name': 'Note', 'ordering': ['member__lname', 'member__fname', '-stamp'], 'verbose_name_plural': 'Notes'},
+        ),
+        migrations.AlterField(
+            model_name='member',
+            name='accepted',
+            field=models.DateField(default=django.utils.timezone.now, verbose_name='Date accepted'),
+        ),
+        migrations.AlterField(
+            model_name='membernote',
+            name='stamp',
+            field=models.DateTimeField(default=django.utils.timezone.now, verbose_name='Datetime', db_index=True),
+        ),
+        migrations.AlterField(
+            model_name='membershipapplication',
+            name='received',
+            field=models.DateField(default=django.utils.timezone.now),
+        ),
+    ]
