31c6471030f88378178a428b0fb0cc02235adf8b
Eero af Heurlin <rambo@iki.fi>
2015-12-26 23:47:00 +0200

    Filter tokens by revokation status and types
    refs #132

--- project/access/admin.py
+++ project/access/admin.py
@@ -9,12 +9,55 @@ class TokenTypeAdmin(VersionAdmin):
     pass
 
 
+class TokenTypeListFilter(admin.SimpleListFilter):
+    title = _("Token types")
+    parameter_name = 'ttype'
+
+    def lookups(self, request, model_admin):
+        return ( (x.pk, x.label) for x in TokenType.objects.all() )
+
+    def queryset(self, request, queryset):
+        v = self.value()
+        if not v:
+            return queryset
+        return queryset.filter(ttype=v)
+
+
+class RevokedListFilter(admin.SimpleListFilter):
+    title = _("Revokation status")
+    parameter_name = 'revoked'
+
+    def lookups(self, request, model_admin):
+        return ( ( -2, _("All")), (None, _("Not revoked")), (1, _("Revoked")) )
+
+    def choices(self, cl):
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': self.value() == lookup,
+                'query_string': cl.get_query_string({
+                    self.parameter_name: lookup,
+                }, []),
+                'display': title,
+            }
+
+    def queryset(self, request, queryset):
+        v = self.value()
+        if v is not None:
+            if int(v) == -2:
+                return queryset
+            if int(v) == 1:
+                return queryset.filter(revoked=True)
+        return queryset.filter(revoked=False)
+
+
 class TokenAdmin(VersionAdmin):
     list_display = (
         'owner',
         'ttype',
         'value_formatted',
     )
+    list_filter = (TokenTypeListFilter, RevokedListFilter)
+
     def value_formatted(self, obj):
         if not obj.revoked:
             return obj.value
