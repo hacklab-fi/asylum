3b0c8c9904874dbd165c06a94d5d5d5f5097d78a
Eero af Heurlin <rambo@iki.fi>
2015-12-26 18:39:42 +0200

    Add fixtures and generation command for grants
    refs #116

---
+++ project/access/management/commands/generate_grants.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand, CommandError
+from access.tests.fixtures.grants import GrantFactory
+
+
+class Command(BaseCommand):
+    help = 'generate randomised (member) access grants'
+
+    def add_arguments(self, parser):
+        parser.add_argument('amount', type=int)
+
+    def handle(self, *args, **options):
+        for i in range(options['amount']):
+            grant = GrantFactory()
+            print("Generated grant %s" % grant)
