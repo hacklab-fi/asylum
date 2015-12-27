919442590507036a9a42d31369ff06b25bbdecd4
Eero af Heurlin <rambo@iki.fi>
2015-12-27 18:30:47 +0200

    Add timescope tests for yearly recurring transactions
    refs #119

---
+++ project/creditor/tests/test_recurring.py
@@ -0,0 +1,51 @@
+import pytest
+import datetime, calendar, pytz
+from creditor.tests.fixtures.recurring import MembershipfeeFactory
+
+def year_start_end(timescope=None):
+    if timescope is None:
+        timescope = datetime.datetime.now()
+    start = start = datetime.datetime(timescope.year, 1, 1).date()
+    end = datetime.datetime(start.year, 12, calendar.monthrange(start.year, 12)[1]).date()
+    return (start, end)
+
+
+def month_start_end(timescope=None):
+    if timescope is None:
+        timescope = datetime.datetime.now()
+    start = datetime.datetime(timescope.year, timescope.month, 1).date()
+    end = datetime.datetime(start.year, start.month, calendar.monthrange(start.year, start.month)[1]).date()
+    return (start, end)
+
+
+@pytest.mark.django_db
+def test_yearly_in_scope_with_end():
+    now = datetime.datetime.now()
+    start, end = year_start_end(now)
+    t = MembershipfeeFactory(start=start, end=end)
+    assert t.in_timescope(now)
+
+
+@pytest.mark.django_db
+def test_yearly_in_scope_without_end():
+    now = datetime.datetime.now()
+    start, end = year_start_end(now-datetime.timedelta(weeks=60))
+    end = None
+    t = MembershipfeeFactory(start=start, end=end)
+    assert t.in_timescope(now)
+
+
+@pytest.mark.django_db
+def test_yearly_not_in_scope_ended():
+    now = datetime.datetime.now()
+    start, end = year_start_end(now-datetime.timedelta(weeks=60))
+    t = MembershipfeeFactory(start=start, end=end)
+    assert not t.in_timescope(now)
+
+
+@pytest.mark.django_db
+def test_yearly_not_in_scope_notstarted():
+    now = datetime.datetime.now()
+    start, end = year_start_end(now+datetime.timedelta(weeks=60))
+    t = MembershipfeeFactory(start=start, end=end)
+    assert not t.in_timescope(now)
