31e42fd5d279fc7808cb8c6fab3f043e2b7d5954
Eero af Heurlin <eero.afheurlin@iki.fi>
2016-01-30 15:48:59 +0200

    Make helper for starting asylum container

---
+++ docker/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+SCRIPTDIR=$(python -c 'import os,sys;print os.path.dirname(os.path.realpath(sys.argv[1]))' "$0")
+
+# Make sure we're in the correct place relative to Dockerfile no matter where we were called from
+cd `dirname "$SCRIPTDIR"`
+
+docker build -t asylum_test .
+echo "Starting test server."
+echo "To connect to shell in this instance run: docker exec -it $(docker ps | grep asylum_test | awk '{print $1}') /bin/bash"
+echo "Then in that shell run: source ../asylum-venv/bin/activate"
+docker run --rm --name asylum_test -it -p 8000:8000 -p 1080:1080 asylum_test
