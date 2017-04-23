3dce48f228caec9b317f263af3c5e7a71372f0be
Eero af Heurlin <eero.afheurlin@iki.fi>
2017-04-23 19:08:29 +0300

    Test that we can get the application form

--- project/members/tests/test_application.py
+++ project/members/tests/test_application.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import pytest
+from django.core.urlresolvers import reverse
 from members.tests.fixtures.memberlikes import MembershipApplicationFactory
 from members.tests.fixtures.types import MemberTypeFactory
 from members.models import Member
@@ -11,3 +12,8 @@ def test_application_approve():
     email = application.email
     application.approve(set_mtypes=mtypes)
     Member.objects.get(email=email)
+
+@pytest.mark.django_db
+def test_get_application_form(client):
+    response = client.get(reverse('members-apply'))
+    assert b'Apply for membership' in response.content
