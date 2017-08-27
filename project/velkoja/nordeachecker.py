28e61588279f1a09a004f49c5dd8f065309f4ae1
Eero af Heurlin <eero.afheurlin@iki.fi>
2017-08-27 18:54:38 +0300

    First attempt at implementation

--- project/velkoja/nordeachecker.py
+++ project/velkoja/nordeachecker.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+import logging
+import datetime
+
 from decimal import Decimal
 
 from django.conf import settings
 from django.core.mail import EmailMessage
+from django.db import models
 from django.template import Context
 from django.template.loader import get_template
 from django.utils import timezone
@@ -10,13 +14,57 @@ from holviapi.utils import barcode as bank_barcode
 from members.models import Member
 from ndaparser.models import UploadedTransaction
 
+
 from .models import NotificationSent
+
+logger = logging.getLogger(__name__)
+
+HOLVI_EXCLUDE_KWARGS = {"reference__startswith": "RF"}
 
 
 class NordeaOverdueInvoicesHandler(object):
     def list_overdue(self):
-        raise NotImplemented()
+        """Gets list of overdue Nordea transactions"""
         # TODO: Heuristics to determine which transactions are not paid...
+        gd = settings.VELKOJA_NORDEACHECKER_GRACE_DAYS
+        if not UploadedTransaction.objects.count():
+            logger.warning("No uploaded transactions found defaulting to configured grace period of {} days".format(gd))
+            cutoff_date = datetime.datetime.now().date() - datetime.timedelta(days=gd)
+        else:
+            cutoff_date = UploadedTransaction.objects.order_by('-last_transaction')[0].last_transaction
+            today_delta = datetime.datetime.now().date() - cutoff_date
+            if today_delta.days > gd:
+                logger.warning("Last uploaded transaction was {} days ago".format(today_delta.days))
+
+        ret = []
+
+        # First get members with negative credit in general
+        members_qs = Member.objects.all()
+        members_qs = members_qs.annotate(credit_annotated=models.Sum('creditor_transactions__amount'))
+        for member in members_qs.filter(credit_annotated__lt=0):
+            # Then find distinct reference numbers that are not Holvi transactions
+            ret += self._list_member_overdue(member, cutoff_date)
+        return ret
+
+    def _list_member_overdue(self, member, cutoff_date):
+        """Figure out which debits of a given member are still unpaid"""
+        base_qs = member.creditor_transactions.exclude(**HOLVI_EXCLUDE_KWARGS).filter(stamp__lte=cutoff_date)
+        refnos = [x['reference'] for x in
+                  base_qs.order_by('reference').distinct('reference').values('reference')]
+        ret = []
+        for refno in refnos:
+            refno_qs = base_qs.filter(reference=refno)
+            refno_credits = refno_qs.filter(amount__gte=0).aggregate(models.Sum('amount'))['amount__sum']
+            refno_debits = refno_qs.filter(amount__lt=0).order_by('stamp')
+            for transaction in refno_debits:
+                refno_credits += transaction.amount  # remember: the amount is negative
+                if refno_credits > 0:
+                    # While we have not expended all credit, keep spending
+                    continue
+                # After no more credits are left, put rest of transactions to the unpaid pile.
+                ret.append(transaction)
+
+        return ret
 
     def process_overdue(self, send=False):
         barcode_iban = settings.NORDEA_BARCODE_IBAN
